1f4e8e43004ff438ae03aac42bf50b627b516638
pre6ter <71014021+pre6ter@users.noreply.github.com>
2020-09-13 20:29:37 +0300

    added tank colliders

---
+++ TowerDefense/Assets/Scripts/MovementTank.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTank : MonoBehaviour
+{
+    public Transform cam;
+    float hor_speed = 35.0f;
+    float ver_speed = 4.0f;
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        transform.Rotate(new Vector3(0, hor_speed * hor * Time.deltaTime));
+        transform.position += transform.forward * -ver_speed * ver * Time.deltaTime;
+        Debug.Log(ver_speed* ver *Time.deltaTime);
+    }
+}
