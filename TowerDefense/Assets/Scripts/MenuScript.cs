9e7227aa2604f0e326395afb0e01028254cdcf1d
Nickolay <47496652+Nezorin@users.noreply.github.com>
2020-10-28 23:07:32 +0300

    Menu added

---
+++ TowerDefense/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("TurretEnemyScene");
+    }
+
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+        Debug.Log("Exit pressed!");//Delete when release
+    }
+}
